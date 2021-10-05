897a50e36fc3909ed73f0d3cf84fddcb01df1879
Yevhenii Tarasiuk <63318800+DatSwif@users.noreply.github.com>
2021-10-05 16:39:21 +0300

    Add files via upload

---
+++ lab_op_2_Csharp/lab_op_2_Csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab_op_3_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Minutes: ");
+            decimal x = Convert.ToDecimal(Console.ReadLine());
+            if (x % 5 > 3)
+            {
+                Console.WriteLine("Red");
+            }
+            else
+            {
+                Console.WriteLine("Green");
+            }
+        }
+    }
+}
---
+++ lab_op_2_python.py
@@ -0,0 +1,5 @@
+x = float(input())
+if x % 5 > 3:
+    print('red')
+else:
+    print('green')
