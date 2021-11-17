2e896a6d613a7aa3cc9d115f875f1271387b6e9c
Yevhenii Tarasiuk <63318800+DatSwif@users.noreply.github.com>
2021-11-17 13:30:32 +0200

    Додано роботу 6 на Python

--- lab_op_6_python.py
+++ lab_op_6_python.py
@@ -1,20 +1,33 @@
 def distance(x,y):
 	return (x*x + y*y)**0.5
 
-i = 1
-count = int(input('how many points?:'))
-print('-------------')
-x = int(input('x{}: '.format(i)))
-y = int(input('y{}: '.format(i)))
-dist_near = distance(x, y)
-ind_near = 1
-for i in range(2, count + 1):
+def getxy():
+        global x, y
         print('-------------')
-        x = int(input('x{}: '.format(i)))
-        y = int(input('y{}: '.format(i)))
-        dist_curr = distance(x, y)
-        if dist_curr < dist_near:
-                dist_near = dist_curr
-                ind_near = i
-print('-------------')
-print('nearest point to (0;0) is', ind_near)
+        x = float(input('x{}: '.format(i)))
+        y = float(input('y{}: '.format(i)))
+
+def getfirst():
+        global i, x, y
+        i = 1
+        count = int(input('how many points?:'))
+        getxy()
+        
+def out():
+        print('-------------')
+        print('nearest point to (0;0) is #', ind_near, sep = '')
+
+def cycle():
+        global dist_near, x, y, dist_curr, ind_near, i
+        ind_near = 1
+        for i in range(2, count + 1):
+                getxy()
+                dist_curr = distance(x, y)
+                if dist_curr < dist_near:
+                        dist_near = dist_curr
+                        ind_near = i
+                        
+getfirst()
+dist_near = distance(x, y)
+cycle()            
+out()
