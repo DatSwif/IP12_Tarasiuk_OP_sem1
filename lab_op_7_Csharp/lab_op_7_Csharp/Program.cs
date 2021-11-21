1abddb45c0424af57e8bf5c4cbd8f879ff562116
Yevhenii Tarasiuk <63318800+DatSwif@users.noreply.github.com>
2021-11-21 22:52:06 +0200

    Додано роботу 7

---
+++ lab_op_7_Csharp/lab_op_7_Csharp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace lab_op_7_Csharp
+{
+    class Program
+    {
+        static int[] form()
+        {
+            var rand = new Random();
+            Console.Write("Size of array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(-999,999);
+            }
+            return array;
+        }
+        static int find_diff(int[] array)
+        {
+            int small = array[0];
+            int big = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < small)
+                {
+                    small = array[i];
+                }
+                else if (array[i] > big)
+                {
+                    big = array[i];
+                }
+            }
+            return big - small;
+        }
+        static void change_the_array(ref int[] array, int diff)
+        {
+            for (int i = 0; i < (array.Length + 1) / 2; i++)
+            {
+                array[i * 2] = diff;
+            }
+        }
+        static void print(int[] array)
+        {
+            string s = "";
+            for(int i = 0; i < array.Length; i++)
+            {
+                s += Convert.ToString(array[i]) + ' ';
+            }
+            Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+            int[] array = form();
+            print(array);
+            int diff = find_diff(array);
+            change_the_array(ref array, diff);
+            print(array);
+        }
+    }
+}
---
+++ lab_op_7_python.py
@@ -0,0 +1,34 @@
+import random
+
+def form():
+    array = []
+    size = int(input('size of array: '))
+    for i in range(size):
+        array.append(random.randint(-999,999))
+    return array
+
+def find_diff(array):
+    small = array[0]
+    big = small
+    for i in range(1, len(array)):
+        if array[i] < small:
+            small = array[i]
+        elif array[i] > big:
+            big = array[i]
+    return big - small
+
+def change_the_array(array, diff):
+    for i in range((len(array) + 1) // 2):
+        array[i*2] = diff
+
+def write(array):
+    string = ''
+    for i in range(len(array)):
+        string += str(array[i]) + ' '
+    print(string)
+
+array = form()
+write(array)
+diff = find_diff(array)
+change_the_array(array, diff)
+write(array)
