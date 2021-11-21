919c71c2aee3e5f9565b8153fb4675a3a1bb38e8
Yevhenii Tarasiuk <63318800+DatSwif@users.noreply.github.com>
2021-11-21 20:37:03 +0200

    Виправлено роботу 6 на Python

--- lab_op_6_python.py
+++ lab_op_6_python.py
@@ -1,33 +1,20 @@
 def distance(x,y):
-	return (x*x + y*y)**0.5
+    return (x*x + y*y)**0.5
 
-def getxy():
-        global x, y
-        print('-------------')
-        x = float(input('x{}: '.format(i)))
-        y = float(input('y{}: '.format(i)))
+def cycle(dist_near):
+    for i in range(2, count + 1):
+        x = int(input('x{}: '.format(i)))
+        y = int(input('y{}: '.format(i)))
+        dist_curr = distance(x, y)
+        if dist_curr < dist_near:
+            dist_near = dist_curr
+            ind_near = i
+    print('nearest point to (0;0) is', ind_near)
 
-def getfirst():
-        global i, x, y
-        i = 1
-        count = int(input('how many points?:'))
-        getxy()
-        
-def out():
-        print('-------------')
-        print('nearest point to (0;0) is #', ind_near, sep = '')
-
-def cycle():
-        global dist_near, x, y, dist_curr, ind_near, i
-        ind_near = 1
-        for i in range(2, count + 1):
-                getxy()
-                dist_curr = distance(x, y)
-                if dist_curr < dist_near:
-                        dist_near = dist_curr
-                        ind_near = i
-                        
-getfirst()
+i = 1
+count = int(input('how many points?:'))
+x = int(input('x{}: '.format(i)))
+y = int(input('y{}: '.format(i)))
 dist_near = distance(x, y)
-cycle()            
-out()
+ind_near = 1
+cycle(dist_near)
