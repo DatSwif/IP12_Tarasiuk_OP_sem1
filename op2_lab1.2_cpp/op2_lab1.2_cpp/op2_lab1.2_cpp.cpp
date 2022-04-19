e2afc8b7b54ad0a97345f0c902cdbb3dd2cc69e3
Yevhenii Tarasiuk <63318800+DatSwif@users.noreply.github.com>
2022-04-19 21:16:51 +0300

    Додано лабораторну роботу 1.2 (Python та C++)
    1.2. Бінарні файли. Варіант 30.
    Створити файл з розкладом занять у навчальному закладі протягом дня: назва предмета, час початку пари (за шаблоном ГГ:ХХ), час закінчення пари. Тривалість пари становить 1 год 45 хв, а перерва між парами - не більше 45 хв. та не менше 45 хв. При введенні даних виконувати перевірку початку кожної пари (чи не накладається на попередню пару та чи дотримані обмеження на тривалість перерви), а кінець пари розраховується автоматично.

---
+++ op2_lab1.2_cpp/op2_lab1.2_cpp/op2_lab1.2_cpp.cpp
@@ -0,0 +1,35 @@
+﻿#include "Header.h"
+
+int main()
+{
+	string filename;
+	cout << "Enter file name:\n";
+	getline(cin, filename);
+	schedule sch;
+	ifstream readFile(filename.c_str(), ios::binary);
+	if (readFile.good()) //file exists
+	{
+		fileToData(readFile, sch);
+		cout << "\nFile contents:\n";
+		toConsole(sch);
+	}
+	else
+	{
+		cout << "\nA new file will be created\n";
+	}
+	readFile.close();
+
+	getNewData(sch);
+
+	ofstream writeFile;
+	writeFile.open(filename, ios::binary);
+	fillFile(writeFile, sch);
+	writeFile.close();
+
+	cout << "\nFile contents:\n";
+	schedule finalData;
+	readFile.open(filename, ios::binary);
+	fileToData(readFile, finalData);
+	readFile.close();
+	toConsole(finalData);
+}
